919949ebe18c0fdb979ed389cf310e24d1346a20
samdan <samdan@whizzogroup.com>
2019-12-24 06:24:58 +0000

    Initial Commit

---
+++ SUISearchBoxTests/SUISearchBoxTests.swift
@@ -0,0 +1,34 @@
+//
+//  SUISearchBoxTests.swift
+//  SUISearchBoxTests
+//
+//  Created by Samuel Daniel on 24/12/2019.
+//  Copyright © 2019 IJIKOD. All rights reserved.
+//
+
+import XCTest
+@testable import SUISearchBox
+
+class SUISearchBoxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
