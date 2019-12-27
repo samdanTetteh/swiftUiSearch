5cafc8e266282e25f04642f4b2bd7ad45182f9b3
samdan <samdan@whizzogroup.com>
2019-12-27 12:32:22 +0000

    added search box

--- SUISearchBox/ContentView.swift
+++ SUISearchBox/ContentView.swift
@@ -9,10 +9,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var searchTerm : String = ""
+    
+    
+    let searchContent = ["google", "twitter", "snapchat", "whatsapp", "instagram", "facebook", "tesla", "microsoft"]
+    
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView{
+            List{
+             
+                SearchBar(text: $searchTerm)
+                ForEach (self.searchContent.filter{
+                    self.searchTerm.isEmpty ? true : $0.localizedCaseInsensitiveContains(self.searchTerm)
+                }, id: \.self) { name in
+                    Text(name)
+                }
+            }.navigationBarTitle(Text("Search Bar"))
+        }
     }
+    
+    
 }
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
